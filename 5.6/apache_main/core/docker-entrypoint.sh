93b526f90724bcb4445ead6f53e0d8f9d9660ae2
juergen-pecher <juergen.pecher@w3pecher.com>
2016-07-18 14:46:54 +0200

    apache php 5.6 with xdebug added

---
+++ 5.6/apache_main/core/docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+# Apache2 custom servername, alias and documentroot
+sed -i "s/MYSERVERNAME/$SERVERNAME/g" /etc/apache2/apache2.conf
+sed -i "s/MYSERVERALIAS/$SERVERALIAS/g" /etc/apache2/apache2.conf
+sed -i "s/MYDOCUMENTROOT/$DOCUMENTROOT/g" /etc/apache2/apache2.conf
