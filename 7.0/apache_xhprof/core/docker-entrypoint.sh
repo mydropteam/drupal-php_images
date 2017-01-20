94bc9ec2211924c478e4baf0f30ba003ee169699
TehesFR <ths.arborisoft@gmail.com>
2017-01-20 15:48:37 +0100

    rollback previous commits

--- 7.0/apache_xhprof/core/docker-entrypoint.sh
+++ 7.0/apache_xhprof/core/docker-entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
-set -e
 # Apache2 custom servername, alias and documentroot
 sed -i "s/MYSERVERNAME/$XHPROF_SERVERNAME/g" /etc/apache2/apache2.conf
 sed -i "s/MYSERVERALIAS/$XHPROF_SERVERALIAS/g" /etc/apache2/apache2.conf
 sed -i "s/MYDOCUMENTROOT/$DOCUMENTROOT/g" /etc/apache2/apache2.conf
 
 # Apache gets grumpy about PID files pre-existing
-rm -f /var/run/apache2/apache2.pid
+ rm -f /var/run/apache2/apache2.pid
 
-chown -R www-data:www-data /var/www/
-# Start Apache in foreground
-/usr/sbin/apache2 -DFOREGROUND
+ chown -R www-data:www-data /var/www/
+ # Start Apache in foreground
+ /usr/sbin/apache2 -DFOREGROUND
